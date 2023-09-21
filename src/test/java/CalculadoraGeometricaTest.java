aec446617e0eaa3f02e5d2926c94a645b393b7b5
HP <f.munoz40@ufromail.cl>
2023-09-20 23:31:07 -0300

    Area, perimetro y volumen

---
+++ src/test/java/CalculadoraGeometricaTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadoraGeometricaTest {
+
+
+    @Test
+    void calculadoraCuadrado() {
+        assertTrue();
+
+    }
+
+    @Test
+    void calculadoraRectangulo() {
+    }
+
+    @Test
+    void circuloCalculadora() {
+    }
+
+    @Test
+    void esferaCalculadora() {
+    }
+
+    @Test
+    void cuboCalculadora() {
+    }
+
+    @Test
+    void conoCalculadora() {
+    }
+}
